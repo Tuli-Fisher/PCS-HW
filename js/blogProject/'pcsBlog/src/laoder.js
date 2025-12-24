7b7a62a46f31918ddb2a10c055b7a72dd7570c0c
tuli fisher <fishertuli@gmail.com>
2025-12-24 14:48:42 -0500

    added cache logic

--- js/blogProject/'pcsBlog/src/laoder.js
+++ js/blogProject/'pcsBlog/src/laoder.js
@@ -1,5 +1,9 @@
 //import { use } from "react";
 
+const userTime = 5 * 60 * 1000;
+const postTime = 3 * 60 * 1000;
+const commentTime = 1 * 60 * 1000;
+
 function saveCache(key, data) {
   localStorage.setItem(
     key,
@@ -10,9 +14,22 @@ function saveCache(key, data) {
   );
 }
 
+function loadCache(key, addedTime) {
+  const cachedData = localStorage.getItem(key);
+  if (cachedData) {
+    const parsedData = JSON.parse(cachedData);
+    if (parsedData.currentTime + addedTime > Date.now()) {
+      console.log(`loading ${key} from cache`);
+      return parsedData.data;
+    }
+  }
+  return null;
+}
+
 export async function loadUsers(url) {
   let userData;
 
+  if ((userData = loadCache("users", userTime)) === null) {
     try {
       console.log("fetching user data");
       const response = await fetch(url);
@@ -24,22 +41,29 @@ export async function loadUsers(url) {
     } catch (e) {
       console.error("Error loading user data:", e);
     }
-  
+  }
 
   return userData;
 }
 
 export async function loadMoreInfo(type, Id) {
-  try {
-    console.log(`fetching ${type} data`);
-    const response = await fetch(
-      `https://jsonplaceholder.typicode.com/${type}?${type}Id=${Id}`
-    );
-    if (!response.ok) {
-      throw new Error(`${response.status} - ${response.statusText}`);
+  let data;
+  const typeTime = type === "users" ? userTime : type === "posts" ? postTime : commentTime;
+  if ((data = loadCache(`${type}-${Id}`, typeTime)) === null) {
+    try {
+      console.log(`fetching ${type} data`);
+      const response = await fetch(
+        `https://jsonplaceholder.typicode.com/${type}?${type}Id=${Id}`
+      );
+      if (!response.ok) {
+        throw new Error(`${response.status} - ${response.statusText}`);
+      }
+      const fetchedData = await response.json();
+      saveCache(`${type}-${Id}`, fetchedData);
+      data = fetchedData;
+    } catch (e) {
+      console.error(`Error loading blog ${type}:`, e);
     }
-    return response.json();
-  } catch (e) {
-    console.error(`Error loading blog ${type}:`, e);
   }
+  return data;
 }
